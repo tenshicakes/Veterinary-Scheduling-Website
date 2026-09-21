48dd20ea1b0f489d9c7aaab290d421bf0e3d3a50
tenshicakes <jmontalan8@gmail.com>
2026-09-22 04:36:31 +0800

    Refactor import statements: reorder LandingPage import for consistency

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
 use App\Http\Middleware\PreventBackHistory;
+use App\Livewire\LandingPage;
 use App\Livewire\Admin\Home as AdminHome;
 use App\Livewire\Assistant\Appointments as AssistantAppointments;
 use App\Livewire\Assistant\Home as AssistantHome;
-use App\Livewire\LandingPage;
 use App\Livewire\Superadmin\Home as SuperadminHome;
 use App\Livewire\User\Appointment as UserAppointment;
 use App\Livewire\User\Home as UserHome;
